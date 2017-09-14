7ef4df87f179655f5f44539154d155ddc7d68bfe
Kevin Sivabalan <kevin.sivabalan@rangle.io>
2017-09-14 16:05:46 -0400

    Prettier Update

--- src/components/Library.vue
+++ src/components/Library.vue
@@ -28,11 +28,7 @@
 </template>
 
 <script>
-import {
-  mapGetters,
-  mapActions,
-  mapMutations,
-} from 'vuex';
+import { mapGetters, mapActions, mapMutations } from 'vuex';
 
 export default {
   name: 'library',
