3dacbf6a8a7c46bc742f29d4942c0a911f5b70e8
Kevin Sivabalan <kevin.sivabalan@rangle.io>
2017-10-07 18:25:52 -0400

    Chart Reactive Not Working

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,7 +1,10 @@
 <script>
-import { Pie } from 'vue-chartjs';
+import { Pie, mixins } from 'vue-chartjs';
+
+const { reactiveProp } = mixins;
 
 export default Pie.extend({
+  mixins: [reactiveProp],
   props: ['data', 'options'],
   mounted() {
     this.renderChart(this.data, this.options);
