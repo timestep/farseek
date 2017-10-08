6099907ffce1aba111945f930f677d017ca68bd8
Kevin Sivabalan <kevin.sivabalan@rangle.io>
2017-10-08 13:18:45 -0400

    Remove Console.log

--- src/store/library/utils.test.js
+++ src/store/library/utils.test.js
@@ -5,7 +5,6 @@ describe('library utils funcs', () => {
     it('check if same card', () => {
       const loadCard = utils.isSameCard({ id: 1 });
       const actual = loadCard({ id: 1 });
-      console.log(loadCard);
       expect(actual).to.equal(true);
     });
   });
